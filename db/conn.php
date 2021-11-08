83051cd5d4450d72bb047f2979fa3db3d45ef478
LorenzoWilliams <LorenzoWilliams@users.noreply.github.com>
2021-11-08 05:54:29 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php
-    $host = '127.0.0.1';
+    //Development connection
+/*     $host = '127.0.0.1';
     $db = 'attendance_db';
     $user = 'root';
     $pass = '';
+    $charset = 'utf8mb4'; */
+
+    //remote connection
+    $host = 'remotemysql.com';
+    $db = 'xESi6TsCzz';
+    $user = 'xESi6TsCzz';
+    $pass = 'YPqlDMmFkm';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
